f9cd3d521b96f3c91d3b21e699749a4675f17dc1
Abhay Prajapati <abhayparja90@gmail.com>
2025-11-09 19:45:42 +0530

    Integrate PrismaModule into AuthModule and enhance AuthService with user signup functionality using PrismaService

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { RegisterDto } from './dto/register.dto';
+import { LoginDto } from './dto/login.dto';
 
 @Injectable()
 export class AuthService {
-    signup(){
-        return "It's for signup"
+    constructor(private prisma: PrismaService) { }
+     signup(data: RegisterDto){
+        const {name,email,password} = data
+        const user =  this.prisma.user.create({
+            data:{
+                name: name,
+                email: email,
+                password: password,
+            }
+        })
+        return user;
     }
 
-    login(){
-        return "It's for login"
+    login(data:LoginDto){
+       const {email ,password} = data
+    //    const user = this.prisma.
     }
 }
